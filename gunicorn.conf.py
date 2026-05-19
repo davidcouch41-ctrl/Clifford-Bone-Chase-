713f21268e046216bcf1c1a48998a868ecab1a28
David Couch <davidcouch41@gmail.com>
2026-05-19 22:21:10 +0000

    tune gunicorn for render stability

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,8 @@ import os
 
 
 bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
-workers = int(os.environ.get("WEB_CONCURRENCY", "2"))
+workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
+worker_tmp_dir = "/dev/shm"
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "120"))
 accesslog = "-"
 errorlog = "-"
